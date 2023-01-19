ed670690bb3fd5a2d9bbb042c97592b52a3baea9
Sankeerthan <sankeerthanmtp@gmail.com>
2023-01-19 17:34:07 +0530

    rectified blank screen because of missing safe area

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNav from './navigation/StackNav';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNav/>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StackNav/>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
