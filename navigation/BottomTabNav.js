b0affcbb1f3caf71010c73bc5c62b177e478c855
Sankeerthan <sankeerthanmtp@gmail.com>
2023-01-19 23:47:56 +0530

    some icon dependancies added

--- navigation/BottomTabNav.js
+++ navigation/BottomTabNav.js
@@ -1,10 +1,26 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons, MaterialCommunityIcons} from '@expo/vector-icons'
+import Login from '../pages/Login';
+import Home from '../pages/Home';
 
+const BottomTab=createBottomTabNavigator()
 export default function BottomTabNav() {
   return (
-    <View>
-      <Text>BottomTabNav</Text>
-    </View>
+    <BottomTab.Navigator
+    screenOptions={{
+        headerShown: false
+    }}
+    >
+      <BottomTab.Screen name='home' component={Home} options={{
+        tabBarLabel: "Home",
+        tabBarIcon: ({color, size})=> <Ionicons name="home" color={color} size={size}/>
+      }}/>
+      <BottomTab.Screen name='account' component={Login} options={{
+        tabBarLabel: "Account",
+        tabBarIcon: ({color, size})=> <MaterialCommunityIcons name="account" color={color} size={size}/>
+      }}/>
+    </BottomTab.Navigator>
   )
 }
--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Text, View} from 'react-native'
 
 export default function Home() {
+    const [data, setData]=useState([])
+    useEffect(()=>{
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then((res)=>res.json())
+        .then((json)=>setData(json))
+    },[])
+
   return (
     <View>
-        <Text>Home</Text>
+        <View>
+            <Text>Categories</Text>
+        </View>
+        {console.log(data)}
     </View>
+   
   )
 }
