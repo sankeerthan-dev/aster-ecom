cf45b15f0b01982c56beb0faa35477c9c004bb6d
Sankeerthan <sankeerthanmtp@gmail.com>
2023-01-22 18:33:16 +0530

    created login page

--- pages/Login.js
+++ pages/Login.js
@@ -1,8 +1,42 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 export default function Login() {
   return (
-    <Text>Login</Text>
+  <View style={{alignItems:'center', backgroundColor:'#ffff', height: '100%'}}>
+  <View style={styles.formContainer}>
+    <TextInput placeholder='Enter email ID' style={styles.inputDesign}/>
+    <TextInput secureTextEntry={true} placeholder='Enter password' style={styles.inputDesign}/>
+    <TouchableOpacity style={styles.loginButton}><Text>Login</Text></TouchableOpacity>
+  </View>
+  </View>
   )
 }
+
+const styles=StyleSheet.create({
+  formContainer:{
+    margin:'auto',
+    // flexDirection: 'column',
+    alignItems: 'center',
+    width: '75%',
+    justifyContent: 'center',
+    marginTop: '60%'
+  },
+  inputDesign:{
+    borderBottomColor: '#047BD5',
+    padding: 5,
+    borderBottomWidth: 3 ,
+    width: '100%',
+    margin: 6
+  },
+  loginButton:{
+    backgroundColor: '#047BD5',
+    paddingTop: 11,
+    alignItems: 'center',
+    borderRadius: 7,
+    width: '35%',
+    color:'#ffff',
+    height: '20%',
+    marginTop: 40
+  }
+})
